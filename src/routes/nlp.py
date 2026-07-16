b151f788e87b7ecd53606bdef4ac9bde537bc9c6
Abdallahwael22 <abdallahwael082@gmail.com>
2026-07-16 03:01:08 +0300

    ollama local llm

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -30,7 +30,8 @@ async def index_project(request:Request,project_id:str, push_request: PushReques
     nlp_controller=NLPController(
         embedding_client=request.app.embedding_client,
         vectordb_client=request.app.vectordb_client,
-        generation_client=request.app.generation_client
+        generation_client=request.app.generation_client,
+        template_parser=request.app.template_parser
     )
 
     has_record=True
@@ -80,7 +81,8 @@ async def get_project_index_info(request: Request,project_id:str):
     nlp_controller=NLPController(
         embedding_client=request.app.embedding_client,
         vectordb_client=request.app.vectordb_client,
-        generation_client=request.app.generation_client
+        generation_client=request.app.generation_client,
+        template_parser=request.app.template_parser
     )
     
     collection_info=nlp_controller.get_vector_db_collection_info(project=project)
